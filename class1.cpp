fcd9399c01b9eef092c4c9d35daf074a9b527b17
Mostafa Radwan <eng.mosradwan@gmail.com>
2017-04-23 00:13:17 +0200

    new file:   class1 	new file:   class1.cpp 	new file:   class1.h

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+#include "class1.h"
+int main () 
+
+{
+class1 car ; 
+
+car.setvalue(10); 
+
+printf ("value is %d",car.getvalue());
+
+
+return 0 ; 
+}
---
+++ class1.h
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <cstdio>
+
+class class1 
+
+{
+
+int i ; 
+
+public :
+
+int getvalue() ; 
+
+void setvalue(int value) ;
+
+
+
+
+};
+
+
+int class1 ::  getvalue() {int x= i ; return x ;}
+void class1 :: setvalue (int value){ i= value;}
+
